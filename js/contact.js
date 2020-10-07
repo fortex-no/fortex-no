b14ac7dde92141fa6e6846cc5d651ac3b867b1f9
Alexandru <rusnac83@yahoo.es>
2020-10-07 16:58:12 +0200

    Added files

---
+++ js/contact.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "-_-");
+
+   
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Kom igjen, du har et navn",
+                    minlength: "Navnet ditt må være minst 2 bokstaver"
+                },
+                subject: {
+                    required: "Kom igjen, du har et emne",
+                    minlength: "Emnet ditt må være minst 4 bokstaver"
+                },
+                email: {
+                    required: "Kom igjen da, du har et email"
+                },
+                message: {
+                    required: "Kom igjen, du må skrive noe",
+                    minlength: "Så lite? Skriv lit mer da"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
