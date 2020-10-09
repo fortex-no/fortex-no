83baf4a5f6a87f473dab03095265b407f6ff38c4
Alexandru <53342591+alexandruromanov@users.noreply.github.com>
2020-10-09 13:46:39 +0200

    Added English pages and refreshed the site

---
+++ js/contact_en.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "-_-");
+
+   
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Come on, you have a name.",
+                    minlength: "Your name must be at least 2 letters."
+                },
+                subject: {
+                    required: "Come on, you have a subject.",
+                    minlength: "Your subject must be at least 4 letters."
+                },
+                email: {
+                    required: "Come on, you have an email."
+                },
+                message: {
+                    required: "Come on, you have something on your mind.",
+                    minlength: "So little? Write a bit more."
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
